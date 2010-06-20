7b1f2b83f2144d31294bc0f3f56ddd4f6a7f32d7
Tim Čas <darkuranium@gmail.com>
2010-06-20 16:52:05 +0000

    Added (WiP) Monster bindings

---
+++ misc/bindings/monster/siege/siege.d
@@ -0,0 +1,12 @@
+module monster.siege.siege;
+
+public import monster.siege.core.game;
+
+class MNSiege
+{
+    MNSiegeCoreGame game;
+    this()
+    {
+        game = new MNSiegeCoreGame;
+    }
+}
