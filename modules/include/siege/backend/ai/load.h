d01649321674e3e7139e550689b4384218d94ec1
Fernando Manuel Fernandes Rodrigues <fmfrodrigues@gmail.com>
2010-07-02 17:48:33 +0000

    AI

---
+++ modules/include/siege/backend/ai/load.h
@@ -0,0 +1,15 @@
+#ifndef __SIEGE_BACKEND_AI_LOAD_H__
+#define __SIEGE_BACKEND_AI_LOAD_H__
+
+#include "../../common.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SIEGE_BACKEND_AI_LOAD_H__
