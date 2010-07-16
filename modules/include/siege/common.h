8adb7150872d1b0d1956698a0da065ac768d3ef0
Tim Čas <darkuranium@gmail.com>
2010-07-16 19:23:39 +0000

    Removed AI module type

--- modules/include/siege/common.h
+++ modules/include/siege/common.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
 typedef void     SGvoid;
 typedef uint8_t  SGbool;
 typedef uint32_t SGenum;
@@ -46,21 +51,24 @@ typedef double SGdouble;
 #define SG_TRUE  1
 #define SG_FALSE 0
 
+#define SG_NAN 0.0f/0.0f
+//#define SG_NAN strtod("NAN", NULL)
+//#define SG_INFINITY
+
 #define SG_OK            0
 #define SG_NO_ERROR      0
 #define SG_UNKNOWN_ERROR 1
 #define SG_INVALID_VALUE 2
 
-#define SG_MODULE_WINDOW       0x0001
-#define SG_MODULE_INPUT        0x0002
+#define SG_MODULE_WINDOW       0x01
+#define SG_MODULE_INPUT        0x02
 #define SG_MODULE_CORE         (SG_MODULE_WINDOW | SG_MODULE_INPUT)
-#define SG_MODULE_GRAPHICS     0x0004
-#define SG_MODULE_GRAPHICSLOAD 0x0008
-#define SG_MODULE_AUDIO        0x0010
-#define SG_MODULE_AUDIOLOAD    0x0020
-#define SG_MODULE_FONTLOAD     0x0040
-#define SG_MODULE_PHYSICS      0x0080
-#define SG_MODULE_AI	       0x0100
+#define SG_MODULE_GRAPHICS     0x04
+#define SG_MODULE_GRAPHICSLOAD 0x08
+#define SG_MODULE_AUDIO        0x10
+#define SG_MODULE_AUDIOLOAD    0x20
+#define SG_MODULE_FONTLOAD     0x40
+#define SG_MODULE_PHYSICS      0x80
 
 #define SG_AUDIO_FORMAT_S8  0x01
 #define SG_AUDIO_FORMAT_S16 0x02
@@ -175,6 +183,10 @@ typedef double SGdouble;
 #define SG_KEYBOARD_KEY_KP_EQUAL    0x315
 #define SG_KEYBOARD_KEY_KP_ENTER    0x316
 
+#define SG_MOUSE_BUTTON_LEFT   1
+#define SG_MOUSE_BUTTON_RIGHT  2
+#define SG_MOUSE_BUTTON_MIDDLE 3
+
 /*typedef struct SGModuleFlags
 {
     // 0-terminated
@@ -200,5 +212,9 @@ typedef struct SGModuleInfo
 
     void* data;
 } SGModuleInfo;
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // __SIEGE_COMMON_H__
