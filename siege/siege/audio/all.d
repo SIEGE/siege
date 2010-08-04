c4824f4608fa8cbd0e793dbe0651623795b2c435
Tim Čas <darkuranium@gmail.com>
2010-08-04 09:08:11 +0000

    Removed DSiege, as it reached EOL

--- siege/siege/audio/all.d
+++
@@ -1,9 +0,0 @@
-module siege.audio.all;
-
-public
-{
-    import siege.audio.buffer;
-    import siege.audio.music;
-    import siege.audio.sample;
-    import siege.audio.source;
-}
--- siege/siege/input/all.d
+++
@@ -1,8 +0,0 @@
-module siege.input.all;
-
-public
-{
-    import siege.input.joystick;
-    import siege.input.keyboard;
-    import siege.input.mouse;
-}
