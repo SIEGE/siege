198aee3bd0834dcf4d9ca3c8fc90eb93f5e1183b
Tim Čas <darkuranium@gmail.com>
2010-10-28 22:09:41 +0200

    Forgot to add the src/doc folder earlier

---
+++ src/doc/main.c
@@ -0,0 +1,14 @@
+/**
+ * \mainpage
+ * Welcome to the SIEGE documentation!
+ *
+ * If you're new to SIEGE, then the \ref tutorials "tutorials" might be a good place to start.
+ *
+ * <hr>
+ *
+ * \section other_res Other resources
+ * \li <a href="http://libsiege.org">http://libsiege.org</a> - home page
+ * \li <a href="http://doc.libsiege.org">http://doc.libsiege.org</a> - documentation
+ * \li <a href="http://wiki.libsiege.org">http://wiki.libsiege.org</a> - wiki
+ * \li <a href="http://bugs.libsiege.org">http://bugs.libsiege.org</a> - bugtracker
+ */
