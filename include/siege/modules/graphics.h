bb55ef7109c165147c04803c7a4f15d8831bcef1
Tim Čas <darkuranium@gmail.com>
2010-10-31 17:04:57 +0100

    Added sgmGraphics[Texture|Surface][Get|Free]Data() in order to support SGMask.

--- include/siege/modules/graphics.h
+++ include/siege/modules/graphics.h
@@ -1,16 +1,16 @@
 /*
-    Copyright (c) 2007 SIEGE Development Team
-    All rights reserved.
-
-    This file is part of libSIEGE.
-
-    This software is copyrighted work licensed under the terms of the
-    2-clause BSD license. Please consult the file "license.txt" for
-    details.
-
-    If you did not recieve the file with this program, please email
-    Tim Chas <darkuranium@gmail.com>.
-*/
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "license.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
 
 #ifndef __SIEGE_MODULES_GRAPHICS_H__
 #define __SIEGE_MODULES_GRAPHICS_H__
@@ -24,67 +24,63 @@ extern "C"
 
 typedef struct SGModuleGraphics
 {
-    /// Graphics
-    SGuint SG_EXPORT (*sgmGraphicsContextCreate)(void** context, SGuint width, SGuint height, SGuint bpp);
-    SGuint SG_EXPORT (*sgmGraphicsContextDestroy)(void* context);
-    SGuint SG_EXPORT (*sgmGraphicsContextResize)(void* context, SGuint width, SGuint height);
-    SGuint SG_EXPORT (*sgmGraphicsContextClear)(void* context, float* color);
+	/// Graphics
+	SGuint SG_EXPORT (*sgmGraphicsContextCreate)(void** context, SGuint width, SGuint height, SGuint bpp);
+	SGuint SG_EXPORT (*sgmGraphicsContextDestroy)(void* context);
+	SGuint SG_EXPORT (*sgmGraphicsContextResize)(void* context, SGuint width, SGuint height);
+	SGuint SG_EXPORT (*sgmGraphicsContextClear)(void* context, float* color);
 
-    SGuint SG_EXPORT (*sgmGraphicsViewportCreate)(void** viewport, void* context);
-    //SGuint SG_EXPORT (*sgmGraphicsViewportCreateView)(void** viewport, void* context, SGuint wx, SGuint wy, SGuint wwidth, SGuint wheight, float x, float y, float width, float height);
-    SGuint SG_EXPORT (*sgmGraphicsViewportDestroy)(void* viewport);
-    SGuint SG_EXPORT (*sgmGraphicsViewportSetView)(void* viewport, SGuint wx, SGuint wy, SGuint wwidth, SGuint wheight, float x, float y, float width, float height);
-    //SGuint SG_EXPORT (*sgmGraphicsViewportGetView)(void* viewport, SGuint* wx, SGuint* wy, SGuint* wwidth, SGuint* wheight, float* x, float* y, float* width, float* height);
+	SGuint SG_EXPORT (*sgmGraphicsViewportCreate)(void** viewport, void* context);
+	//SGuint SG_EXPORT (*sgmGraphicsViewportCreateView)(void** viewport, void* context, SGuint wx, SGuint wy, SGuint wwidth, SGuint wheight, float x, float y, float width, float height);
+	SGuint SG_EXPORT (*sgmGraphicsViewportDestroy)(void* viewport);
+	SGuint SG_EXPORT (*sgmGraphicsViewportSetView)(void* viewport, SGuint wx, SGuint wy, SGuint wwidth, SGuint wheight, float x, float y, float width, float height);
+	//SGuint SG_EXPORT (*sgmGraphicsViewportGetView)(void* viewport, SGuint* wx, SGuint* wy, SGuint* wwidth, SGuint* wheight, float* x, float* y, float* width, float* height);
 
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceCreate)(void** surface, void* context);
-    //SGuint SG_EXPORT (*sgmGraphicsSurfaceCreateTexture)(void** surface, void* context, void* texture);
-    //SGuint SG_EXPORT (*sgmGraphicsSurfaceCreateData)(void** surface, void* context, SGuint width, SGuint height, SGuint bpp, void* data);
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceDestroy)(void* surface);
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceSetTexture)(void* surface, void* texture);
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceGetTexture)(void* surface, void** texture);
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceSetData)(void* surface, SGuint width, SGuint height, SGuint bpp, void* data);
-    //
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceGetData)(void* surface, SGuint* width, SGuint* height, SGuint* bpp, void** data);
-    //
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceFreeData)(void* surface, void* data);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceCreate)(void** surface, void* context);
+	//SGuint SG_EXPORT (*sgmGraphicsSurfaceCreateTexture)(void** surface, void* context, void* texture);
+	//SGuint SG_EXPORT (*sgmGraphicsSurfaceCreateData)(void** surface, void* context, SGuint width, SGuint height, SGuint bpp, void* data);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceDestroy)(void* surface);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceSetTexture)(void* surface, void* texture);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceGetTexture)(void* surface, void** texture);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceSetData)(void* surface, SGuint width, SGuint height, SGuint bpp, void* data);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceGetData)(void* surface, SGuint* width, SGuint* height, SGuint* bpp, void** data);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceFreeData)(void* surface, void* data);
 
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceGetSize)(void* surface, SGuint* width, SGuint* height);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceGetSize)(void* surface, SGuint* width, SGuint* height);
 
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceDraw)(void* surface, float x, float y, float z, float xscale, float yscale, float xoffset, float yoffset, float angle);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceDraw)(void* surface, float x, float y, float z, float xscale, float yscale, float xoffset, float yoffset, float angle);
 
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceSetTarget)(void* surface);
-    //
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceGetTarget)(void** surface);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceSetTarget)(void* surface);
+	//SGuint SG_EXPORT (*sgmGraphicsSurfaceGetTarget)(void** surface);
 
-    SGuint SG_EXPORT (*sgmGraphicsSurfaceResetTarget)(void* surface);
+	SGuint SG_EXPORT (*sgmGraphicsSurfaceResetTarget)(void* surface);
 
 
-    SGuint SG_EXPORT (*sgmGraphicsTextureCreate)(void** texture, void* context);
-    //
-    SGuint SG_EXPORT (*sgmGraphicsTextureCreateData)(void** texture, void* context, SGuint width, SGuint height, SGuint bpp, void* data);
+	SGuint SG_EXPORT (*sgmGraphicsTextureCreate)(void** texture, void* context);
+	//SGuint SG_EXPORT (*sgmGraphicsTextureCreateData)(void** texture, void* context, SGuint width, SGuint height, SGuint bpp, void* data);
 
-    SGuint SG_EXPORT (*sgmGraphicsTextureDestroy)(void* texture);
-    SGuint SG_EXPORT (*sgmGraphicsTextureSetData)(void* texture, SGuint width, SGuint height, SGuint bpp, void* data);
-    //SGuint SG_EXPORT (*sgmGraphicsTextureGetData)(void* texture, SGuint* width, SGuint* height, SGuint* bpp, void** data);
-    //SGuint SG_EXPORT (*sgmGraphicsTextureFreeData)(void* data);
-    SGuint SG_EXPORT (*sgmGraphicsTextureGetSize)(void* texture, SGuint* width, SGuint* height);
-    SGuint SG_EXPORT (*sgmGraphicsTextureDraw)(void* texture, float x, float y, float z, float xscale, float yscale, float xoffset, float yoffset, float angle);
+	SGuint SG_EXPORT (*sgmGraphicsTextureDestroy)(void* texture);
+	SGuint SG_EXPORT (*sgmGraphicsTextureSetData)(void* texture, SGuint width, SGuint height, SGuint bpp, void* data);
+	SGuint SG_EXPORT (*sgmGraphicsTextureGetData)(void* texture, SGuint* width, SGuint* height, SGuint* bpp, void** data);
+	SGuint SG_EXPORT (*sgmGraphicsTextureFreeData)(void* data);
+	SGuint SG_EXPORT (*sgmGraphicsTextureGetSize)(void* texture, SGuint* width, SGuint* height);
+	SGuint SG_EXPORT (*sgmGraphicsTextureDraw)(void* texture, float x, float y, float z, float xscale, float yscale, float xoffset, float yoffset, float angle);
 
-    SGuint SG_EXPORT (*sgmGraphicsDrawPrimitive)(void* context, void* texture, SGuint type, SGuint numverts, float* points, float* texcoords, float* colors);
-    SGuint SG_EXPORT (*sgmGraphicsDrawSetColor)(void* context, float* color);
-    SGuint SG_EXPORT (*sgmGraphicsDrawPointSetSize)(void* context, float size);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawPointGetSize)(void* context, float* size);
-    SGuint SG_EXPORT (*sgmGraphicsDrawLineSetWidth)(void* context, float size);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawLineGetWidth)(void* context, float* size);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawLineSetStipple)(void* context, SGuint factor, SGuint pattern);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawLineGetStipple)(void* context, SGuint* factor, SGuint* pattern);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawPolygonSetStipple)(void* context, SGubyte* pattern);
-    //SGuint SG_EXPORT (*sgmGraphicsDrawPolygonGetStipple)(void* context*, SGubyte** pattern);
+	SGuint SG_EXPORT (*sgmGraphicsDrawPrimitive)(void* context, void* texture, SGuint type, SGuint numverts, float* points, float* texcoords, float* colors);
+	SGuint SG_EXPORT (*sgmGraphicsDrawSetColor)(void* context, float* color);
+	SGuint SG_EXPORT (*sgmGraphicsDrawPointSetSize)(void* context, float size);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawPointGetSize)(void* context, float* size);
+	SGuint SG_EXPORT (*sgmGraphicsDrawLineSetWidth)(void* context, float size);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawLineGetWidth)(void* context, float* size);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawLineSetStipple)(void* context, SGuint factor, SGuint pattern);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawLineGetStipple)(void* context, SGuint* factor, SGuint* pattern);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawPolygonSetStipple)(void* context, SGubyte* pattern);
+	//SGuint SG_EXPORT (*sgmGraphicsDrawPolygonGetStipple)(void* context*, SGubyte** pattern);
 
-    /// Graphics Load
-    SGuint SG_EXPORT (*sgmGraphicsLoadFile)(const char* fname, SGuint* width, SGuint* height, SGuint* bpp, void** data);
-    //SGuint SG_EXPORT (*sgmGraphicsLoadStream)(void* stream, SGuint* width, SGuint* height, SGuint* bpp, void** data);
-    SGuint SG_EXPORT (*sgmGraphicsLoadFreeData)(void* data);
+	/// Graphics Load
+	SGuint SG_EXPORT (*sgmGraphicsLoadFile)(const char* fname, SGuint* width, SGuint* height, SGuint* bpp, void** data);
+	//SGuint SG_EXPORT (*sgmGraphicsLoadStream)(void* stream, SGuint* width, SGuint* height, SGuint* bpp, void** data);
+	SGuint SG_EXPORT (*sgmGraphicsLoadFreeData)(void* data);
 } SGModuleGraphics;
 
 #ifdef SG_BUILD_LIBRARY
--- src/siege/modules/graphics.c
+++ src/siege/modules/graphics.c
@@ -1,16 +1,16 @@
 /*
-    Copyright (c) 2007 SIEGE Development Team
-    All rights reserved.
-
-    This file is part of libSIEGE.
-
-    This software is copyrighted work licensed under the terms of the
-    2-clause BSD license. Please consult the file "license.txt" for
-    details.
-
-    If you did not recieve the file with this program, please email
-    Tim Chas <darkuranium@gmail.com>.
-*/
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "license.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
 
 #define SG_BUILD_LIBRARY
 #include <siege/util/link.h>
@@ -23,113 +23,113 @@
 
 void SG_EXPORT _sgModuleLoadGraphics(SGLibrary* lib)
 {
-    void* fptr;
-    /// Graphics
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsContextCreate);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsContextDestroy);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsContextResize);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsContextClear);
+	void* fptr;
+	/// Graphics
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsContextCreate);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsContextDestroy);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsContextResize);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsContextClear);
 
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsViewportCreate);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportCreateView);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsViewportDestroy);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsViewportSetView);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportGetView);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportCreate);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportCreateView);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportDestroy);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportSetView);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsViewportGetView);
 
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreate);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreateTexture);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreateData);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceDestroy);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetTexture);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetTexture);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetData);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetData);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceFreeData);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetSize);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceDraw);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetTarget);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetTarget);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceResetTarget);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreate);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreateTexture);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceCreateData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceDestroy);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetTexture);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetTexture);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceFreeData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetSize);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceDraw);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceSetTarget);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceGetTarget);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsSurfaceResetTarget);
 
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsTextureCreate);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureCreateData);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsTextureDestroy);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsTextureSetData);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureGetData);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureFreeData);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsTextureGetSize);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsTextureDraw);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureCreate);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureCreateData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureDestroy);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureSetData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureGetData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureFreeData);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureGetSize);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsTextureDraw);
 
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsDrawPrimitive);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsDrawSetColor);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsDrawPointSetSize);
-    //bindFunc(sgmGraphicsDrawPointGetSize)("sgmGraphicsDrawPointGetSize", lib);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsDrawLineSetWidth);
-    //bindFunc(sgmGraphicsDrawLineGetWidth)("sgmGraphicsDrawLineGetWidth", lib);
-    //bindFunc(sgmGraphicsDrawLineSetStipple)("sgmGraphicsDrawLineSetStipple", lib);
-    //bindFunc(sgmGraphicsDrawLineGetStipple)("sgmGraphicsDrawLineGetStipple", lib);
-    //bindFunc(sgmGraphicsDrawPolygonSetStipple)("sgmGraphicsDrawPolygonSetStipple", lib);
-    //bindFunc(sgmGraphicsDrawPolygonGetStipple)("sgmGraphicsDrawPolygonGetStipple", lib);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsDrawPrimitive);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsDrawSetColor);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsDrawPointSetSize);
+	//bindFunc(sgmGraphicsDrawPointGetSize)("sgmGraphicsDrawPointGetSize", lib);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsDrawLineSetWidth);
+	//bindFunc(sgmGraphicsDrawLineGetWidth)("sgmGraphicsDrawLineGetWidth", lib);
+	//bindFunc(sgmGraphicsDrawLineSetStipple)("sgmGraphicsDrawLineSetStipple", lib);
+	//bindFunc(sgmGraphicsDrawLineGetStipple)("sgmGraphicsDrawLineGetStipple", lib);
+	//bindFunc(sgmGraphicsDrawPolygonSetStipple)("sgmGraphicsDrawPolygonSetStipple", lib);
+	//bindFunc(sgmGraphicsDrawPolygonGetStipple)("sgmGraphicsDrawPolygonGetStipple", lib);
 
-    /// Graphics Load
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsLoadFile);
-    //_SG_BINDS(_sg_modGraphics, sgmGraphicsLoadStream);
-    _SG_BINDS(_sg_modGraphics, sgmGraphicsLoadFreeData);
+	/// Graphics Load
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsLoadFile);
+	//_SG_BINDS(_sg_modGraphics, sgmGraphicsLoadStream);
+	_SG_BINDS(_sg_modGraphics, sgmGraphicsLoadFreeData);
 }
 
 SGModuleGraphics* SG_EXPORT sgModuleGetGraphics(void)
 {
-    return &_sg_modGraphics;
+	return &_sg_modGraphics;
 }
 
 SGuint SG_EXPORT _sgNextPower2(SGuint num)
 {
-    SGuint i;
-    for(i = 2; i < num; i <<= 1)
-    {
-    }
-    return i;
+	SGuint i;
+	for(i = 2; i < num; i <<= 1)
+	{
+	}
+	return i;
 }
 
 SGubyte* SG_EXPORT _sgGraphicsToCanvasSize(SGuint width, SGuint height, SGuint bpp, SGubyte* data)
 {
-    SGuint awidth = _sgNextPower2(width);
-    SGuint aheight = _sgNextPower2(height);
-    SGuint bypp;
-    switch(bpp)
-    {
-        case 0:
-            fprintf(stderr, "Getting bpp from window unimplemented, using 24\n");
-            bypp = 3;
-            break;
-        case 8:
-            bypp = 1;
-            break;
-        case 15:
-            bypp = 2;
-            break;
-        case 16:
-            bypp = 2;
-            break;
-        case 24:
-            bypp = 3;
-            break;
-        case 32:
-            bypp = 4;
-            break;
-        default:
-            fprintf(stderr, "Bad bid depth\n");
-            return NULL;
-    }
-    SGubyte* newData = malloc(awidth * aheight * bypp);
-    if((awidth == width) && (aheight == height))
-        return memcpy(newData, data, width * height * bypp);
+	SGuint awidth = _sgNextPower2(width);
+	SGuint aheight = _sgNextPower2(height);
+	SGuint bypp;
+	switch(bpp)
+	{
+		case 0:
+			fprintf(stderr, "Getting bpp from window unimplemented, using 24\n");
+			bypp = 3;
+			break;
+		case 8:
+			bypp = 1;
+			break;
+		case 15:
+			bypp = 2;
+			break;
+		case 16:
+			bypp = 2;
+			break;
+		case 24:
+			bypp = 3;
+			break;
+		case 32:
+			bypp = 4;
+			break;
+		default:
+			fprintf(stderr, "Bad bid depth\n");
+			return NULL;
+	}
+	SGubyte* newData = malloc(awidth * aheight * bypp);
+	if((awidth == width) && (aheight == height))
+		return memcpy(newData, data, width * height * bypp);
 
-    SGuint x;
-    SGuint y;
-    for(y = 0; y < height; y++)
-        for(x = 0; x < width; x++)
-            memcpy(&newData[bypp * (x + y * awidth)], &data[bypp * (x + y * width)], bypp);
-    return newData;
+	SGuint x;
+	SGuint y;
+	for(y = 0; y < height; y++)
+		for(x = 0; x < width; x++)
+			memcpy(&newData[bypp * (x + y * awidth)], &data[bypp * (x + y * width)], bypp);
+	return newData;
 }
 
