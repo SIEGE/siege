0f221ac40a5667bd3ac1d5f2e2601674deb9c63b
Tim Čas <darkuranium@gmail.com>
2010-11-19 22:53:42 +0100

    Fixed some commit errors from before.

--- src/modules/SGModule-OpenGL/texture.h
+++ src/modules/SGModule-OpenGL/texture.h
@@ -1,16 +1,16 @@
 /*
-    Copyright (c) 2007 SIEGE Development Team
-    All rights reserved.
-
-    This file is part of libSIEGE.
-
-    This software is copyrighted work licensed under the terms of the
-    2-clause BSD license. Please consult the file "license.txt" for
-    details.
-
-    If you did not recieve the file with this program, please email
-    Tim Chas <darkuranium@gmail.com>.
-*/
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "license.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
 
 #ifndef __TEXTURE_H__
 #define __TEXTURE_H__
@@ -26,6 +26,7 @@ typedef struct TextureData
     float wratio, hratio;
     SGubyte bpp;
     GLuint glbpp;
+	SGenum glformat;
 } TextureData;
 
 #endif // __TEXTURE_H__
