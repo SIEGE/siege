aea6c013c3dc9b8d356a0ae50710b75cf84a5f02
Gabriel Pettier <gabriel.pettier@gmail.com>
2011-08-01 23:07:35 +0200

    implemented _sgParticleCreate

--- src/siege/graphics/particles.c
+++ src/siege/graphics/particles.c
@@ -1,15 +1,15 @@
 /*
-    Copyright (c) 2007 SIEGE Development Team
-    All rights reserved.
-
-    This file is part of libSIEGE.
-
-    This software is copyrighted work licensed under the terms of the
-    2-clause BSD license. Please consult the file "license.txt" for
-    details.
-
-    If you did not recieve the file with this program, please email
-    Tim Chas <darkuranium@gmail.com>.
+ *  Copyright (c) 2007 SIEGE Development Team
+ *  All rights reserved.
+ *
+ *  This file is part of libSIEGE.
+ *
+ *  This software is copyrighted work licensed under the terms of the
+ *  2-clause BSD license. Please consult the file "license.txt" for
+ *  details.
+ *
+ *  If you did not recieve the file with this program, please email
+ *  Tim Chas <darkuranium@gmail.com>.
 */
 
 
@@ -17,5 +17,21 @@
 #include <siege/graphics/particles.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <malloc.h>
+
+/*
+ * TODO:
+ * implement everything in particles.h!
+ */
+
+SGParticle* _sgParticleCreate(float x, float y, float angle, float speed)
+{
+	SGParticle* particle = (SGParticle*) malloc(sizeof(SGParticle));
+	particle->x = x;
+	particle->y = y;
+	particle->angle = angle;
+	particle->speed = speed;
+	return particle;
+}
 
 
