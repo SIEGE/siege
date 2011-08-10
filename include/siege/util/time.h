ac2a12637ac9475efcbe88703f2e06db100fe6ce
Bernard Helyer <b.helyer@gmail.com>
2011-08-10 23:58:06 +1200

    Added util/time.c.
    Only implemented on Linux currently.

---
+++ include/siege/util/time.h
@@ -0,0 +1,26 @@
+/*
+    Copyright (c) 2007 SIEGE Development Team
+    All rights reserved.
+
+    This file is part of libSIEGE.
+
+    This software is copyrighted work licensed under the terms of the
+    2-clause BSD license. Please consult the file "license.txt" for
+    details.
+
+    If you did not recieve the file with this program, please email
+    Tim Chas <darkuranium@gmail.com>.
+*/
+#ifndef __SIEGE_UTIL_TIME_H__
+#define __SIEGE_UTIL_TIME_H__
+
+#include <siege/common.h>
+
+/**
+ * Function: sgGetTime
+ *
+ * Returns: nanoseconds from a fixed, unspecified, origin.
+ */
+SGlong SG_EXPORT sgGetTime(void);
+
+#endif // __SIEGE_UTIL_TIME_H__
---
+++ src/siege/util/time.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "license.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+#include <siege/util/time.h>
+
+#ifdef linux
+#include <time.h>
+
+SGlong SG_EXPORT sgGetTime(void)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_sec * 1000000000L) + ts.tv_nsec;
+}
+#else
+#warning "sgGetTime unimplemented on your platform."
+SGlong SG_EXPORT sgGetTime(void)
+{
+    return 0L;
+}
+#endif
