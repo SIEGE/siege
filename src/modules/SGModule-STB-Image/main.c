f298a53700aacc1df0b291c4fae12250cd706cf1
Tim Čas <darkuranium@gmail.com>
2012-09-25 15:34:00 +0200

    Added SGModule-STB-Image.

---
+++ src/modules/SGModule-STB-Image/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "COPYING.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+
+#include <siege/backend.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+SGenum SG_EXPORT sgmModuleInit(SGModuleInfo** minfo)
+{
+    *minfo = (SGModuleInfo*) calloc(1, sizeof(SGModuleInfo));
+    (*minfo)->vmajor = SG_VERSION_MAJOR;
+    (*minfo)->vminor = SG_VERSION_MINOR;
+    (*minfo)->vpatch = SG_VERSION_PATCH;
+
+    (*minfo)->name = "STB-Image";
+
+    return SG_OK;
+}
+
+SGenum SG_EXPORT sgmModuleExit(SGModuleInfo* minfo)
+{
+    free(minfo);
+    return SG_OK;
+}
+
+SGenum SG_EXPORT sgmModuleMatch(SGModuleInfo** minfos, SGuint numinfos, SGbool* ok)
+{
+    *ok = SG_TRUE; // we're independent of other modules...
+    return SG_OK;
+}
