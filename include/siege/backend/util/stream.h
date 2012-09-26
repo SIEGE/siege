5ad2ee10617a427cb16e083a885811df5e0ecf09
Tim Čas <darkuranium@gmail.com>
2012-09-26 20:45:53 +0200

    Argh... Forgot a file AGAIN.

---
+++ include/siege/backend/util/stream.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "COPYING.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+
+#ifndef __SIEGE_BACKEND_UTIL_STREAM_H__
+#define __SIEGE_BACKEND_UTIL_STREAM_H__
+
+#include "../../util/stream.h"
+
+#endif /* __SIEGE_BACKEND_UTIL_STREAM_H__ */
