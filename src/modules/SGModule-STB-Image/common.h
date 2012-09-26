a3fa233f158838023603fa639cbcfd78d0ea32fc
Tim Čas <darkuranium@gmail.com>
2012-09-26 20:32:30 +0200

    Added a file I forgot to add earlier

---
+++ src/modules/SGModule-STB-Image/common.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "COPYING.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+
+#ifndef __COMMON__
+#define __COMMON__
+
+#include <siege/backend.h>
+#include "stb_image.h"
+
+stbi_io_callbacks callbacks;
+
+#endif /* __COMMON__ */
