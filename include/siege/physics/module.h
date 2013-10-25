0f8344ca676f9f4ab78a4d74439ab3ab030ec0da
Tim Čas <darkuranium@gmail.com>
2013-10-25 22:40:49 +0200

    Added missing `#ifdef __cplusplus`.

--- include/siege/physics/module.h
+++ include/siege/physics/module.h
@@ -4,6 +4,10 @@
 #include "../common.h"
 #include "../module.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 SGbool SG_CALL SG_FPTR(_sgPhysicsInit)(void);
 SGbool SG_CALL SG_FPTR(_sgPhysicsDeinit)(void);
 
@@ -11,5 +15,9 @@ SGbool SG_CALL _sgPhysicsLoad(void);
 SGbool SG_CALL _sgPhysicsUnload(void);
 
 SGbool SG_CALL sgPhysicsIsAvailable(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* __SG_PHYSICS_MODULE_H__ */
